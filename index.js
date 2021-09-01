124880bada264aa7b2ffc440b70e304b33cef7c7
akhil <akhil_pando@outlook.com>
2021-09-01 13:21:32 +0530

    add input box for height and width

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-function drawRectangles(ctx,NoRect,X,Y,height,width){
+function drawRectangles(ctx,NoRect,X,Y,width,height){
+
     console.log("drawing rectangles")
-    for(let i =0 ;i< NoRect; i++){
-        X = X + ctx.lineWidth * 2;
-        Y = Y + ctx.lineWidth * 2;
-        height = height - ctx.lineWidth * 4;
-        width = width - ctx.lineWidth * 4;
+    ctx.beginPath();
+    ctx.rect(X, Y, width, height);
+    var grd = ctx.createRadialGradient(75, 50, 30, 90, 60, 100);
+    grd.addColorStop(0.9, "white");
+    grd.addColorStop(1, "violet");
+    ctx.fillStyle = grd;
+    ctx.fill();
+    for(let i =0 ;i< NoRect-1; i++){
+        X = X + 15;
+        Y = Y + 15;
+        height = height - 30;
+        width = width - 30;
         ctx.beginPath();
         ctx.rect(X, Y, width, height);
         ctx.fill();
-        ctx.lineWidth = 5;
-        var color = Math.floor((Math.random() * X+Y));
-        ctx.strokeStyle ='hsl('+color+',100%,50%)'
+        ctx.lineWidth = 10;
+        // var color = Math.floor((Math.random() * X+Y));
+        ctx.strokeStyle = 'white'
         ctx.stroke();
     }
     
@@ -23,10 +31,5 @@ function init() {
     let NoRect = document.getElementById('Norect').value;
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    drawRectangles(ctx,NoRect,200,100,height,width);
+    drawRectangles(ctx,NoRect,200,100,width,height);
 }
-
-
-
-
-
