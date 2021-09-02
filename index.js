c8bd81e497f10b239dab90f7ef6dd333ec146d2c
akhil <akhil_pando@outlook.com>
2021-09-02 14:05:10 +0530

    created a recursive function to draw rectangles

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-function drawRectangles(ctx,NoRect,X,Y,width,height){
-
-    console.log("drawing rectangles")
-    ctx.beginPath();
-    ctx.rect(X, Y, width, height);
-    var grd = ctx.createRadialGradient(75, 50, 30, 90, 60, 100);
-    grd.addColorStop(0.9, "white");
-    grd.addColorStop(1, "violet");
-    ctx.fillStyle = grd;
-    ctx.fill();
-    for(let i =0 ;i< NoRect-1; i++){
-        X = X + 15;
-        Y = Y + 15;
-        height = height - 30;
-        width = width - 30;
+function drawRectangles(ctx,NoRect,X,Y,width,height,color){
+    
+    if (NoRect > 0 && width > 1 && height > 1) {
+        var count = []
         ctx.beginPath();
         ctx.rect(X, Y, width, height);
+        color = color + 5;
+        count.push(color)
+        ctx.fillStyle = "hsl(" + color + ",100%,50%)";
         ctx.fill();
-        ctx.lineWidth = 10;
-        // var color = Math.floor((Math.random() * X+Y));
-        ctx.strokeStyle = 'white'
-        ctx.stroke();
-    }
-    
+        X = X + 10;
+        Y = Y + 10;
+        width = width - 20;
+        height = height - 20;
+        NoRect = NoRect - 1;
+        drawRectangles(ctx, NoRect, X, Y, width, height, color);
+      }
 }
 
 
@@ -31,5 +24,5 @@ function init() {
     let NoRect = document.getElementById('Norect').value;
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    drawRectangles(ctx,NoRect,200,100,width,height);
+    drawRectangles(ctx,NoRect,200,100,width,height,1000);
 }
